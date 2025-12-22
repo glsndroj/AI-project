85a06d391c7ea8d6e43bc34f6f43ddfae545f3a8
glsndroj <galsandooorj@gmail.com>
2025-12-22 10:33:17 +0800

    neznayu

--- app/_components/_features/ImageAnalysis.tsx
+++ app/_components/_features/ImageAnalysis.tsx
@@ -11,29 +11,63 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import ImageAnalysisSvg from "@/SVG/ImageAnalysis";
+import { errorMonitor } from "events";
 import { useState } from "react";
 
-
 export default function ImageAnalysis() {
-  const [uploadedImage, setUploadedImage] = useState<File | null>(null)
-  const [uploadedImageUrl, setUploadedImageUrl] = useState("")
-  const [detectedObject, setDetectedobject] = useState<any[]>([])
-  const [analyzing, setAnalyzing] = useState(false)
+  const [uploadedImage, setUploadedImage] = useState<File | null>(null);
+  const [uploadedImageUrl, setUploadedImageUrl] = useState("");
+  const [detectedObject, setDetectedobject] = useState<any[]>([]);
+  const [analyzing, setAnalyzing] = useState(false);
+
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setUploadedImage(file);
+      const url = URL.createObjectURL(file);
+      setDetectedobject([]);
+    }
+  };
+
+  const analyzeImage = async () => {
+    if (!uploadedImage) return;
+    setAnalyzing(true);
+    setDetectedobject([]);
+    try {
+      const formData = new FormData()
+      formData.append("image", uploadedImage);
+
+      const response = await fetch("api/gemini", {
+        method: "POST",
+        body: formData,
+      })
+      if(response.ok) {
+        const data = await response.json();
+        setDetectedobject(data.objects || []
+        )
+      }else {
+        console.error("Failed to analyze image")
+      }
+    } catch (error) {
+      console.error("Error: ", error)
+    }finally{
+      setAnalyzing(false)
+    }
+  };
 
   return (
     <Card className="h-fit w-[580px]">
       <CardHeader>
         <CardTitle>
-          <ImageAnalysisSvg/>
+          <ImageAnalysisSvg />
         </CardTitle>
         <CardDescription>
-       Upload a food photo, and AI will detect the ingredients.
+          Upload a food photo, and AI will detect the ingredients.
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-6">
         <div className="grid gap-3">
-          
-          <Input type="file"/>
+          <Input type="file" />
         </div>
       </CardContent>
       <CardFooter className="flex justify-end">
